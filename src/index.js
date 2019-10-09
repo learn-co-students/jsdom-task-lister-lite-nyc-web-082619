b967adc9b31c21ecc828afee9b5c1dcb3dcbc572
Tania Manco <belalov.tania16@gmail.com>
2019-10-09 12:47:14 -0400

    adding functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const form = document.querySelector("#create-task-form")
+  const description = document.querySelector("#new-task-description")
+  const list = document.querySelector("#list")
+  const tasks = document.querySelector("#tasks")
+
+ form.addEventListener("submit", function(event) {
+   event.preventDefault()
+   tasks.insertAdjacentHTML('beforeend',`<li> ${description.value} <button onclick="this.parentElement.remove()">Delete</button></li>`)
+ });
+
+
+ 
+})
